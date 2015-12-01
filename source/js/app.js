b9438e664a986d65d1fcc61fe4897584edb1c10f
Jason Lu <jlu@squiz.net>
2015-12-01 11:11:59 +1100

    updated to laest markup, added live api call to staff and map

--- source/js/app.js
+++ source/js/app.js
@@ -1,80 +1,36 @@
 'use strict';
 
+// function to grab query string
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
 var React = require('react'),
     Profile = require('modules/people/Profile.js'),
+    requestProfileId = getParameterByName('id'),
+    profileData = {};
 
-    //this array will be fetched from a JSAPI call when that is integrated
-    profileData = {
-        "data" : {
-          "givenName" : "MICHAEL",
-          "additionalName" : null,
-          "familyName" : "LO",
-          "preferredName" : "MICHAEL",
-          "honorificPrefix" : "MR",
-          "jobTitle" : "INTEGRATION DESIGNER",
-          "peopleGroup" : "Professional Continuing",
-          "affiliation" : "Project Services",
-          "telephone" : [ "+61390355806" ],
-          "mobile" : [ "0414967502" ],
-          "email" : [ "as-testsupport@unimelb.edu.au" ],
-          "memberOf" : null,
-          "locationCampus" : null,
-          "workLocation" : null,
-          "buildingNumber" : null,
-          "locationFloor" : null,
-          "locationRoom" : null,
-          "isExpert" : "N",
-          "supervisor" : {
-            "givenName" : "HANS",
-            "additionalName" : "TOMMY",
-            "familyName" : "HOEGLUND",
-            "preferredName" : "TOMMY",
-            "honorificPrefix" : "MR",
-            "position" : "MANAGER, IT SYSTEMS INTEGRATION",
-            "affiliation" : "Professional Continuing",
-            "telephone" : [ "+61390355806" ],
-            "mobile" : [ "0414967502" ],
-            "email" : [ "as-testsupport@unimelb.edu.au" ],
-            "personID" : "079181"
-          },
-          "colleagues" : [ {
-            "givenName" : "GAGANDEEP",
-            "additionalName" : null,
-            "familyName" : "SINGH",
-            "preferredName" : "GAGANDEEP SINGH",
-            "honorificPrefix" : "MR",
-            "position" : "INTEGRATION DEVELOPER",
-            "affiliation" : "Professional Continuing",
-            "email" : [ "as-testsupport@unimelb.edu.au" ],
-            "personID" : "57084"
-          }, {
-            "givenName" : "MANEESHA",
-            "additionalName" : null,
-            "familyName" : "MANI",
-            "preferredName" : "MANEESHA",
-            "honorificPrefix" : "MRS",
-            "position" : "INTEGRATION DEVELOPER",
-            "affiliation" : "Professional Continuing",
-            "telephone" : [ "+61390355196" ],
-            "email" : [ "as-testsupport@unimelb.edu.au" ],
-            "personID" : "195234"
-          }, {
-            "givenName" : "ALIAKBAR",
-            "additionalName" : null,
-            "familyName" : "VOHRA",
-            "preferredName" : null,
-            "honorificPrefix" : "MR",
-            "position" : null,
-            "affiliation" : "Non Payroll",
-            "email" : [ "as-testsupport@unimelb.edu.au" ],
-            "personID" : "111538"
-          } ]
-        }
-      };
+(function($) {
+    //define staff API URL
+    var staffAPI = "http://staff.unimelb.edu.au/dev/people-search-new/staff-details-api-call?id=" + requestProfileId;
+    //ajax and grab json object
 
-//instantiating top level components
-$('.profile-div').each(function(){
-    var div         = this,
-        profile = React.createElement( Profile, { profile: profileData});
-        React.render(profile, div);
-});
+    $.getJSON( staffAPI, {
+      format: "json"
+    })
+    .done(function( data ) {
+      profileData = data;
+      //render react profile component
+      console.log(profileData);
+      $('.profile-div').each(function(){
+          var div = this,
+              profile = React.createElement( Profile, { profile: profileData});
+              React.render(profile, div);
+      });
+      window.UOMbind('icons');
+    });
+
+})(jQuery);
--- source/modules/people/Profile.js
+++ source/modules/people/Profile.js
@@ -7,85 +7,94 @@ var React = require('react'),
 // Profile Module
 //--------------------------------------------------------------------------------------
     Profile = React.createClass({
-
-      /*  getInitialState: function() {
-            return {
-                discussionGroup: this.props.discussionGroup
-            };
-        },*/
+        //function to append markup if condition is true
         appendIfTrue: function(base, toAppend, test) {
             if (!test) { return base; }
             return [base].concat(toAppend).join(' ');
         },
         getInitialState: function(){
+          //set profile image and building Map url states
           return {
-            profileImage: ''
+            profileImage: '',
+            buildingMapCoord: 'test'
           }
         },
         componentWillMount: function(){
-          //check whether profile image exists
-          var profileID = this.props.profile.data.personID;
-          var profileImageURL = 'https://findanexpert.unimelb.edu.au/pictures/thumbnail'+profileID+'picture';
-          var profile = this;
-          CheckImage(profileImageURL).then(
-              function(url){
-                profile.setState({profileImage: profileImageURL })
-              }).catch(function(url){
-                profile.setState({profileImage: "https://staff.unimelb.edu.au/directory/images/placeholder.png"})
-              })
+            console.log(this.props.profile);
+            var profileID = this.props.profile.data.personId;
+            var buildingID = this.props.profile.data.buildingNumber;
+            var profileImageURL = 'https://findanexpert.unimelb.edu.au/pictures/thumbnail'+profileID+'picture';
+            var profile = this;
+            //Check if image exists, else use placeholder image
+            CheckImage(profileImageURL).then(
+                function(url){
+                  profile.setState({profileImage: profileImageURL })
+                }).catch(function(url){
+                  profile.setState({profileImage: "https://staff.unimelb.edu.au/directory/images/placeholder.png"})
+                })
+
+            //ajax map API
+            //define map API URL
+            var mapAPI = "https://api.its.unimelb.edu.au/services/maps?service=WFS&version=1.0&outputFormat=json&request=GetFeature&maxFeatures=2000&typeName=uom:by-building&viewparams=bldgid:" + buildingID;
+            //ajax and grab json object
+            $.getJSON(mapAPI)
+            .done(function( data ) {
+                if(data.features[0].geometry.coordinates){
+                    profile.setState({buildingMapCoord: data.features[0].geometry.coordinates})
+                }
+            });
         },
-        render: function() {
-            var profileData = this.props.profile.data; //grab profile data from property
-            var profileName = profileData.givenName +" "+ profileData.familyName;
+        render: function() { //render profile
+            var profileData = this.props.profile.data, //grab profile data from property
+                profileName = profileData.givenName +" "+ profileData.familyName,
+                profilePhotoStyle = {backgroundImage: 'url('+ this.state.profileImage +')'},
+                buildingMapURL = "http://www.google.com.au/maps?hl=en&amp;q="+this.state.buildingMapCoord[0]+","+this.state.buildingMapCoord[1];
             return (
               <div>
-                <header className="person">
-                    <div className="avatar">
-                        <img alt={profileName} src={this.state.profileImage} />
-                    </div>
-                    <div className="detail">
-                        <h1>{profileData.givenName} {profileData.familyName}</h1>
+                <header className="profile-header">
+                    <div className="profile-header__summary">
+                        <div className="profile-header__photo" style={profilePhotoStyle}></div>
+                        <h1>{profileName}</h1>
                         <p><em>{profileData.position}</em></p>
-                        <p className="last">{profileData.affiliation}</p>
-                        <div className="contact">
-                            {profileData.telephone ?<p>
-                                <span className="small" data-icon="phone" data-bound="true">
-                                    <span dangerouslySetInnerHTML={{__html: '<svg class="icon" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-phone"></use></svg>'}} />
-                                    <span className="icon-over"></span>
-                                </span>
-                                <a href="tel:{profileData.telephone}">{profileData.telephone}</a>
-                            </p>:''}
-                            {profileData.mobile ?<p>
-                                <span className="small" data-icon="smartphone" data-bound="true">
-                                    <span dangerouslySetInnerHTML={{__html: '<svg class="icon" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-smartphone"></use></svg>'}} />
-                                    <span className="icon-over"></span>
-                                </span>
-                                <a href="tel:{profileData.mobile}">{profileData.mobile}</a>
-                            </p>:''}
-                            {profileData.email ?<p>
-                                <span className="small" data-icon="mail" data-bound="true">
-                                    <span dangerouslySetInnerHTML={{__html: '<svg class="icon" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-mail"></use></svg>'}} />
-                                    <span className="icon-over"></span>
-                                </span>
-                                <a href="mailto:{profileData.email}">{profileData.email}</a>
-                            </p>: ''}
-                            {profileData.isExpert === 'Y' ?<p>
-                                <span className="small" data-icon="profile" data-bound="true">
-                                  <span dangerouslySetInnerHTML={{__html: '<svg class="icon" role="img"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#icon-profile"></use></svg>'}} />
-                                  <span className="icon-over"></span>
-                                </span>
-                                <a href="http://findanexpert.unimelb.edu.au/display/person{profileData.isExpert}">Find an Expert profile</a>
-                            </p>: ''}
-                        </div>
-                        <div className="location">
-                            <p><span className="label">Room:</span> G16</p>
-                            <p><span className="label">Floor:</span> GR</p>
-                            <p><span className="label">Building:</span> <a href="http://www.google.com.au/maps?hl=en&amp;q=-37.79772,144.96075">33 Lincoln Square Sth</a></p>
-                            <p><span className="label">Campus:</span> Parkville</p>
-                        </div>
+                        <p>{profileData.departmentName}</p>
+                    </div>
+                    <div className="profile-header__info">
+                        <ul className="profile-header__contact">
+                            {profileData.telephone ?
+                                <li>
+                                  <span className="icon--hide-label" data-icon="phone">Phone number</span>
+                                  <a href="tel:{profileData.telephone}"> {profileData.telephone}</a>
+                                </li>
+                            :''}
+                            {profileData.mobile ?
+                                <li>
+                                  <span className="icon--hide-label" data-icon="smartphone">Mobile number</span>
+                                  <a href="tel:{profileData.mobile}"> {profileData.mobile}</a>
+                                </li>
+                            :''}
+                            {profileData.email ?
+                                <li>
+                                  <span className="icon--hide-label" data-icon="mail">Email</span>
+                                  <a href="mailto:{profileData.email}"> {profileData.email}</a>
+                                </li>
+                            :''}
+                            {profileData.faeExists === 'Y' ?
+                                <li>
+                                  <span data-icon="profile"></span> <a href="http://findanexpert.unimelb.edu.au/display/person{profileData.personId}">Find an Expert profile</a>
+                                </li>
+                            :''}
+                        </ul>
+                        <ul className="profile-header__location">
+                          {/* display location information */}
+                          {profileData.locationRoom ? <li><em>Room:</em> {profileData.locationRoom}</li> :''}
+                          {profileData.locationFloor ? <li><em>Floor:</em> {profileData.locationFloor}</li> :''}
+                          {profileData.buildingNumber ? <li><em>Building:</em> <a href={buildingMapURL}>{profileData.buildingNumber}</a></li> : ''}
+                          {profileData.locationCampus ? <li><em>Campus:</em> {profileData.locationCampus}</li> :''}
+                        </ul>
                     </div>
                 </header>
                 <PeopleList name="Supervisor" people={profileData.supervisor} />
+                <PeopleList name="Direct Reports" people={profileData.reports} />
                 <PeopleList name="Colleagues" people={profileData.colleagues} />
               </div>
             );
@@ -99,17 +108,25 @@ var React = require('react'),
         render: function() {
             return (
               <div>
+                  { this.props.people ? //check if people list is not empty
+                        this.props.people.constructor === Array ?
+                            this.props.people.length > 1 ?
+                              <h2>{this.props.name} ({this.props.people.length})</h2>
+                           :  <h2>{this.props.name}</h2>
+                         : <h2>{this.props.name}</h2>
+                    : ''
+                  }
 
-
-              {this.props.people ?
-                this.props.people.constructor === Array ?
-                  this.props.people.map(function(person, i) {
-                      return <div><h2>this.props.name</h2><ol className='search-results'><Person key={i} personData={person} /></ol></div>
-                  })
-                : <div><h2>this.props.name</h2><ol className='search-results'><Person personData={this.props.people} /></ol></div>
-              : ''
-              }
-
+                  <ol className='search-results'>
+                  {this.props.people ? //check if people list is not empty
+                    this.props.people.constructor === Array ? //check if list contains more than one person (array)
+                      this.props.people.map(function(person, i) { //if array, process each object
+                          return <Person personData={person} key={i} />
+                      })
+                    :<Person personData={this.props.people} />
+                  : ''
+                  }
+                  </ol>
               </div>
             );
         }
@@ -121,11 +138,12 @@ var React = require('react'),
     Person = React.createClass({
         getInitialState: function(){
           return {
-            profileImage: ''
+            profileImage: '' //set profile image state
           }
         },
         componentWillMount: function(){
-          var profileID = this.props.personData.personID;
+          //Check if image exists, else use placeholder image
+          var profileID = this.props.personData.personId;
           var profileImageURL = 'https://findanexpert.unimelb.edu.au/pictures/thumbnail'+profileID+'picture';
           var person = this;
           CheckImage(profileImageURL).then(
@@ -137,13 +155,18 @@ var React = require('react'),
         },
         render: function() {
             var personData = this.props.personData;
+            // set div background image style as object
+            var profilePhotoStyle = {backgroundImage: 'url('+ this.state.profileImage +')'};
+            //get current URL
+            var profileUrl = '//' + location.host + location.pathname+"?id="+personData.personId;
+            //return person listing markup
             return (
                 <li className="person">
-                    <img alt="" className="person__photo" height="85" src={this.state.profileImage} width="85" />
+                    <div className="person__photo" style={profilePhotoStyle}></div>
                     <div className="person__info">
                         <div className="person__profile">
-                            <h3><a href="#" data-bound="true">{personData.givenName} {personData.additionalName} {personData.familyName}</a></h3>
-                            <p><em>{personData.position}</em></p><p>{personData.affiliation}</p>
+                            <h3><a href={profileUrl} data-bound="true">{personData.givenName} {personData.additionalName} {personData.familyName}</a></h3>
+                            <p><em>{personData.position}</em></p><p>{personData.departmentName}</p>
                         </div>
                         <div className="person__contact">
                             {personData.telephone ? <p className="person__phone"><a href="tel:{personData.telephone}">{personData.telephone}</a></p>:''}
