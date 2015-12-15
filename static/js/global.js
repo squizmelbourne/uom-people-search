b8f49609b6cd4780edb26d1d39158068d4c3781b
Jason Lu <jlu@squiz.net>
2015-12-15 15:39:38 +1100

    pushing static for GIT Bridge

---
+++ static/js/global.js
@@ -0,0 +1,36 @@
+/*global jQuery*/
+/**
+ * people-search
+ * Global JS
+ *
+ * version: 0.0.1
+ * file:    global.js
+ * author:  Squiz Australia
+ * change log:
+ *     Tue Dec 15 2015 15:39:10 GMT+1100 (AEDT) - First revision
+ */
+
+/*
+ * Table of Contents
+ *
+ * - Global
+ * - Modules
+
+ */
+'use strict';
+/*
+--------------------
+Global
+--------------------
+*/
+//  Declare JS Enabled.
+$('body').removeClass('no-js').addClass('js-enabled');
+
+
+/*
+--------------------
+Modules
+--------------------
+*/
+
+//# sourceMappingURL=global.js.map
